60f21edb207026e3210a81b382fe528f75fad81a
Aleksandr Popovkin <catharsis96@yandex.ru>
2016-12-07 13:39:32 +0000

    22

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -4,14 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //test function
-            //new
+            //
         }
     }
 }
